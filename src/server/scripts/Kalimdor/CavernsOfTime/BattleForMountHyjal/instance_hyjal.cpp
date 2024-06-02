a9e15b7bb056c464471bc3cbdf21e42bc54500a4
kissingers <kissinger@foxmail.com>
2024-06-02 23:58:19 +0800

    修复海山小怪战斗失败后，NPC死亡复活时间从300s改为118s

--- src/server/scripts/Kalimdor/CavernsOfTime/BattleForMountHyjal/instance_hyjal.cpp
+++ src/server/scripts/Kalimdor/CavernsOfTime/BattleForMountHyjal/instance_hyjal.cpp
@@ -419,7 +419,7 @@ public:
                         if (Creature* creature = instance->GetCreature(guid))
                             creature->DespawnOrUnsummon();
 
-                    _scheduler.Schedule(300s, [this](TaskContext)
+                    _scheduler.Schedule(118s, [this](TaskContext)
                         {
                             for (ObjectGuid const& guid : _baseAlliance)
                                 if (Creature* creature = instance->GetCreature(guid))
@@ -442,7 +442,7 @@ public:
                         if (Creature* creature = instance->GetCreature(guid))
                             creature->DespawnOrUnsummon();
 
-                    _scheduler.Schedule(300s, [this](TaskContext)
+                    _scheduler.Schedule(118s, [this](TaskContext)
                         {
                             for (ObjectGuid const& guid : _baseHorde)
                                 if (Creature* creature = instance->GetCreature(guid))
@@ -462,7 +462,7 @@ public:
 
                     GetCreature(DATA_ARCHIMONDE)->DespawnOrUnsummon();
 
-                    _scheduler.Schedule(300s, [this](TaskContext)
+                    _scheduler.Schedule(118s, [this](TaskContext)
                         {
                             for (ObjectGuid const& guid : _baseNightElf)
                                 if (Creature* creature = instance->GetCreature(guid))
